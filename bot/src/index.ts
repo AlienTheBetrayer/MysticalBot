ff6a1ca2b34c6700c1c73307e1619fc6afba7489
AlienTheBetrayer <Alien@TheBetrayer.com>
2021-01-21 21:05:32 +0200

    still start

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Start.");
+// Setup
+// Importing all the dependencies
+import * as Discord from "discord.js";
+
+// Configuration files
+import config from "../config.json";
+import private_config from "../private_config.json";
+
+// Initializing the client
+const client: Discord.Client = new Discord.Client();
+
+const events = new Map(); // name : event
+const commands = new Map(); // [name, aliases...] : { command }
+// Inputting all the commands
+// ...
+
+// Inputting all the events
+// ...
+
+// Logging in the client
+client.login(private_config.token);
